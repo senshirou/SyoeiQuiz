22350784b8578c5b1fb82a3e4b8d2d5642f87c9e
senshirou <senshirou@hotmail.com>
2019-08-02 15:18:32 +0900

    合っていない答えを選択しても〇になる不具合を修正

--- Assets/Script/AnswerButton.cs
+++ Assets/Script/AnswerButton.cs
@@ -18,59 +18,64 @@ public class AnswerButton : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI AnswerA;
 
-    public string Quiz1 = "±0.1";
-    public string Quiz2 = "±0.1";
-    public string Quiz3 = "±0.08";
-    public string Quiz4 = "±0.15";
-    public string Quiz5 = "±0.1";
-    public string Quiz6 = "±0.1";
-    public string Quiz7 = "±0.15";
-    public string Quiz8 = "30μ以下";
-    public string Quiz9 = "±25μ";
-    public string Quiz10 = "3μ以上";
-    public string Quiz11 = "0.8μ以上";
-    public string Quiz12 = "3個";
-    public string Quiz13 = "25%";
-    public string Quiz14 = "±0.05";
-    public string Quiz15 = "±0.05";
-    public string Quiz16 = "0.02";
-    public string Quiz17 = "±0.2";
-    public string Quiz18 = "0.02";
-    public string Quiz19 = "0.05";
-    public string Quiz20 = "0.02";
-    public string Quiz21 = "0.02";
-    public string Quiz22 = "400を超えるものは、100までの寸法増加ごとに0.05を加える";
-    public string Quiz23 = "±0.1";
-    public string Quiz24 = "±0.05";
-    public string Quiz25 = "±0.05";
-    public string Quiz26 = "0.1";
-    public string Quiz27 = "w:最小導体間げきを満足し、かつ設計導体間げき値の20%以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に1個以下、かつ100×100mm中に2個までOK";
-    public string Quiz28 = "w:仕上がり幅の25%以下 ℓ:仕上がり導体幅以下  個数:X:3y3";
-    public string Quiz29 = "w:仕上がり幅の20%以下 ℓ:仕上がり導体幅以下  個数:X=1 Y=2";
-    public string Quiz30 = "w:最小導体間げきを満足し、かつ設計導体間げき値の20%以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に1個以下、かつ100×100mm中に2個までOK";
-    public string Quiz31 = "w:0.2mm以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に2個以下、かつ100×100mm中に3個までOK";
-    public string Quiz32 = "欠損面積の割合:ランド面積の25%以下 、(p):0.05mm以上、(q):0.15mm以上";
-    public string Quiz33 = "(a):20%　(b):0.2mm";
-    public string Quiz34 = "数字をランダムにする為保留。";
-    public string Quiz35 = "数字をランダムにする為保留。";
-    public string Quiz36 = "0個";
-    public string Quiz37 = "反り:1.0 ねじれ:1.5";
-    public string Quiz38 = "8μ";
-    public string Quiz39 = "(a):0.05mm以上 (b)穴の接線までは可。";
-    public string Quiz40 = "レジストの傷";
-    public string Quiz41 = "インク塗布面の色ムラ";
-    public string Quiz42 = "0.15";
-    public string Quiz43 = "0.8";
-    public string Quiz44 = "文字、記号のかすれ、にじみはあってはならない";
-    public string Quiz45 = "0.8μ";
-    public string Quiz46 = "±0.2";
-    public string Quiz47 = "50までの寸法増加毎に0.1加える。";
-    public string Quiz48 = "6.0";
-    public string Quiz49 = "0.4";
-    public string Quiz50 = "3ヶ月";
-    public string Quiz51 = "6ヶ月";
-    public string Quiz52 = "ベーキング処理して再フラックス";
-    public string Quiz53 = "ベーキング処理";
+    TextMeshProUGUI QuizNumber;
+
+    string Quiz1 = "±0.1";
+    string Quiz2 = "±0.1";
+    string Quiz3 = "±0.08";
+    string Quiz4 = "±0.15";
+    string Quiz5 = "±0.1";
+    string Quiz6 = "±0.1";
+    string Quiz7 = "±0.15";
+    string Quiz8 = "30μ以下";
+    string Quiz9 = "±25μ";
+    string Quiz10 = "3μ以上";
+    string Quiz11 = "0.8μ以上";
+    string Quiz12 = "3個";
+    string Quiz13 = "25%";
+    string Quiz14 = "±0.05";
+    string Quiz15 = "±0.05";
+    string Quiz16 = "0.02";
+    string Quiz17 = "±0.2";
+    string Quiz18 = "0.02";
+    string Quiz19 = "0.05";
+    string Quiz20 = "0.02";
+    string Quiz21 = "0.02";
+    string Quiz22 = "400を超えるものは、100までの寸法増加ごとに0.05を加える";
+    string Quiz23 = "±0.1";
+    string Quiz24 = "±0.05";
+    string Quiz25 = "±0.05";
+    string Quiz26 = "0.1";
+    string Quiz27 = "w:最小導体間げきを満足し、かつ設計導体間げき値の20%以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に1個以下、かつ100×100mm中に2個までOK";
+    string Quiz28 = "w:仕上がり幅の25%以下 ℓ:仕上がり導体幅以下  個数:X:3y3";
+    string Quiz29 = "w:仕上がり幅の20%以下 ℓ:仕上がり導体幅以下  個数:X=1 Y=2";
+    string Quiz30 = "w:最小導体間げきを満足し、かつ設計導体間げき値の20%以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に1個以下、かつ100×100mm中に2個までOK";
+    string Quiz31 = "w:0.2mm以下。ℓ:その部分の設計導体間げき値を超えてはならない。個数:隣接する導体間に2個以下、かつ100×100mm中に3個までOK";
+    string Quiz32 = "欠損面積の割合:ランド面積の25%以下 、(p):0.05mm以上、(q):0.15mm以上";
+    string Quiz33 = "(a):20%　(b):0.2mm";
+    string Quiz34 = "数字をランダムにする為保留。";
+    string Quiz35 = "数字をランダムにする為保留。";
+    string Quiz36 = "0個";
+    string Quiz37 = "反り:1.0 ねじれ:1.5";
+    string Quiz38 = "8μ";
+    string Quiz39 = "(a):0.05mm以上 (b)穴の接線までは可。";
+    string Quiz40 = "レジストの傷";
+    string Quiz41 = "インク塗布面の色ムラ";
+    string Quiz42 = "0.15";
+    string Quiz43 = "0.8";
+    string Quiz44 = "文字、記号のかすれ、にじみはあってはならない";
+    string Quiz45 = "0.8μ";
+    string Quiz46 = "±0.2";
+    string Quiz47 = "50までの寸法増加毎に0.1加える。";
+    string Quiz48 = "6.0";
+    string Quiz49 = "0.4";
+    string Quiz50 = "3ヶ月";
+    string Quiz51 = "6ヶ月";
+    string Quiz52 = "ベーキング処理して再フラックス";
+    string Quiz53 = "ベーキング処理";
+
+    QuizPrototype Quiztype;
+
 
 
 
@@ -93,15 +98,18 @@ public class AnswerButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        QuizNumber = GameObject.Find("QuizNumber").GetComponent<TextMeshProUGUI>();
+        Quiztype = GameObject.Find("QuizPrototype").GetComponent<QuizPrototype>();
         float weight = quiz.weight;
         Debug.Log(weight);
-        collect = new string[]{"エッチング →　酸洗　→　フラックス","だめだ","Android",$"{0.2f + weight}mm",Quiz1, Quiz2,  Quiz3,  Quiz4,  Quiz5,  Quiz6,  Quiz7,  Quiz8,  Quiz9,  Quiz10, Quiz11, Quiz12, Quiz13, Quiz14, Quiz15, Quiz16, Quiz17, Quiz18, Quiz19, Quiz20, Quiz21, Quiz22, Quiz23, Quiz24, Quiz25, Quiz26, Quiz27, Quiz28, Quiz29, Quiz30, Quiz31, Quiz32, Quiz33, Quiz34, Quiz35, Quiz36, Quiz37, Quiz38, Quiz39, Quiz40, Quiz41, Quiz42, Quiz43, Quiz44, Quiz45, Quiz46, Quiz47, Quiz48, Quiz49, Quiz50, Quiz51, Quiz52, Quiz53};
+        //collect = new string[]{"エッチング →　酸洗　→　フラックス","だめだ","Android",$"{0.2f + weight}mm",Quiz1, Quiz2,  Quiz3,  Quiz4,  Quiz5,  Quiz6,  Quiz7,  Quiz8,  Quiz9,  Quiz10, Quiz11, Quiz12, Quiz13, Quiz14, Quiz15, Quiz16, Quiz17, Quiz18, Quiz19, Quiz20, Quiz21, Quiz22, Quiz23, Quiz24, Quiz25, Quiz26, Quiz27, Quiz28, Quiz29, Quiz30, Quiz31, Quiz32, Quiz33, Quiz34, Quiz35, Quiz36, Quiz37, Quiz38, Quiz39, Quiz40, Quiz41, Quiz42, Quiz43, Quiz44, Quiz45, Quiz46, Quiz47, Quiz48, Quiz49, Quiz50, Quiz51, Quiz52, Quiz53};
     }
 
     
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Quiztype.questionIndex);
         
     }
 
@@ -117,29 +125,94 @@ public class AnswerButton : MonoBehaviour
         Text AnsButton = GetComponentInChildren<Text>();
 
 
-        //ボタンのテキストと正解の配列を判定
-        for (int i = 0; i < collect.Length; i++)
-        {
 
-            if(AnsButton.text == collect[i])
-            {
-                //テキストと正解の配列がイコールならtrueにする。
-                hantei = true;
-            }
-        }
+
+        if (QuizNumber.text == "Quiz1") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz1) { hantei = true; } }
+        if (QuizNumber.text == "Quiz2") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz2) { hantei = true; } }
+        if (QuizNumber.text == "Quiz3") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz3) { hantei = true; } }
+        if (QuizNumber.text == "Quiz4") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz4) { hantei = true; } }
+        if (QuizNumber.text == "Quiz5") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz5) { hantei = true; } }
+        if (QuizNumber.text == "Quiz6") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz6) { hantei = true; } }
+        if (QuizNumber.text == "Quiz7") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz7) { hantei = true; } }
+        if (QuizNumber.text == "Quiz8") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz8) { hantei = true; } }
+        if (QuizNumber.text == "Quiz9") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz9) { hantei = true; } }
+        if (QuizNumber.text == "Quiz10") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz10) { hantei = true; } }
+        if (QuizNumber.text == "Quiz11") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz11) { hantei = true; } }
+        if (QuizNumber.text == "Quiz12") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz12) { hantei = true; } }
+        if (QuizNumber.text == "Quiz13") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz13) { hantei = true; } }
+        if (QuizNumber.text == "Quiz14") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz14) { hantei = true; } }
+        if (QuizNumber.text == "Quiz15") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz15) { hantei = true; } }
+        if (QuizNumber.text == "Quiz16") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz16) { hantei = true; } }
+        if (QuizNumber.text == "Quiz17") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz17) { hantei = true; } }
+        if (QuizNumber.text == "Quiz18") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz18) { hantei = true; } }
+        if (QuizNumber.text == "Quiz19") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz19) { hantei = true; } }
+        if (QuizNumber.text == "Quiz20") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz20) { hantei = true; } }
+        if (QuizNumber.text == "Quiz21") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz21) { hantei = true; } }
+        if (QuizNumber.text == "Quiz22") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz22) { hantei = true; } }
+        if (QuizNumber.text == "Quiz23") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz23) { hantei = true; } }
+        if (QuizNumber.text == "Quiz24") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz24) { hantei = true; } }
+        if (QuizNumber.text == "Quiz25") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz25) { hantei = true; } }
+        if (QuizNumber.text == "Quiz26") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz26) { hantei = true; } }
+        if (QuizNumber.text == "Quiz27") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz27) { hantei = true; } }
+        if (QuizNumber.text == "Quiz28") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz28) { hantei = true; } }
+        if (QuizNumber.text == "Quiz29") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz29) { hantei = true; } }
+        if (QuizNumber.text == "Quiz30") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz30) { hantei = true; } }
+        if (QuizNumber.text == "Quiz31") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz31) { hantei = true; } }
+        if (QuizNumber.text == "Quiz32") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz32) { hantei = true; } }
+        if (QuizNumber.text == "Quiz33") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz33) { hantei = true; } }
+        if (QuizNumber.text == "Quiz34") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz34) { hantei = true; } }
+        if (QuizNumber.text == "Quiz35") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz35) { hantei = true; } }
+        if (QuizNumber.text == "Quiz36") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz36) { hantei = true; } }
+        if (QuizNumber.text == "Quiz37") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz37) { hantei = true; } }
+        if (QuizNumber.text == "Quiz38") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz38) { hantei = true; } }
+        if (QuizNumber.text == "Quiz39") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz39) { hantei = true; } }
+        if (QuizNumber.text == "Quiz40") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz40) { hantei = true; } }
+        if (QuizNumber.text == "Quiz41") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz41) { hantei = true; } }
+        if (QuizNumber.text == "Quiz42") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz42) { hantei = true; } }
+        if (QuizNumber.text == "Quiz43") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz43) { hantei = true; } }
+        if (QuizNumber.text == "Quiz44") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz44) { hantei = true; } }
+        if (QuizNumber.text == "Quiz45") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz45) { hantei = true; } }
+        if (QuizNumber.text == "Quiz46") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz46) { hantei = true; } }
+        if (QuizNumber.text == "Quiz47") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz47) { hantei = true; } }
+        if (QuizNumber.text == "Quiz48") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz48) { hantei = true; } }
+        if (QuizNumber.text == "Quiz49") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz49) { hantei = true; } }
+        if (QuizNumber.text == "Quiz50") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz50) { hantei = true; } }
+        if (QuizNumber.text == "Quiz51") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz51) { hantei = true; } }
+        if (QuizNumber.text == "Quiz52") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz52) { hantei = true; } }
+        if (QuizNumber.text == "Quiz53") { Debug.Log("HIT"); if (AnsButton.text == Quiztype.Quiz53) { hantei = true; } }
+
+
+
+
+
+
+
+        //ボタンのテキストと正解の配列を判定
+        //for (int i = 0; i < collect.Length; i++)
+        //{
+
+        //    if(AnsButton.text == collect[i])
+        //    {
+        //        //テキストと正解の配列がイコールならtrueにする。
+        //        hantei = true;
+        //    }
+        //}
 
         //hanteiがtrueの場合〇を出力
-        if(hantei == true)
+        if (hantei == true)
         {
             Answermaru();
-           
+            QuizNumber.text = "";
+
+
         }
 
         //そうでない場合は×出力
         else
         {
             Answerbatu();
-            
+            QuizNumber.text = "";
+
         }
         
         
