108c890476c6194a593880470ca0c34cbe0fe86f
senshirou <senshirou@hotmail.com>
2019-08-16 14:56:51 +0900

    add a question picture

--- Assets/Script/ImageControl.cs
+++ Assets/Script/ImageControl.cs
@@ -22,11 +22,12 @@ public class ImageControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (QuizNumber.text == "Quiz3" || QuizNumber.text == "Quiz4")
+        if (QuizNumber.text == "Quiz3" || QuizNumber.text == "Quiz4" || QuizNumber.text == "Quiz5")
         {
             GetComponent<Image>().color = new Color32(255, 255, 255, 255);
             sprite = Resources.Load<Sprite>("Question3_4png");
             QuizImage.sprite = sprite;
+            size.sizeDelta = new Vector2(697.7f, 312.6f);
         }
 
         else if(QuizNumber.text == "Quiz8")
@@ -35,8 +36,31 @@ public class ImageControl : MonoBehaviour
             sprite = Resources.Load<Sprite>("Question8");
             QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(242.5f, 266.5f);
-            
+           
+        }
 
+        else if (QuizNumber.text == "Quiz9")
+        {
+            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            sprite = Resources.Load<Sprite>("Question9");
+            QuizImage.sprite = sprite;
+            size.sizeDelta = new Vector2(697.7f, 312.6f);
+        }
+
+        else if (QuizNumber.text == "Quiz12")
+        {
+            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            sprite = Resources.Load<Sprite>("Question12");
+            QuizImage.sprite = sprite;
+            size.sizeDelta = new Vector2(353.6f, 312.6f);
+        }
+
+        if (QuizNumber.text == "Quiz14" || QuizNumber.text == "Quiz15")
+        {
+            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            sprite = Resources.Load<Sprite>("Question14_15");
+            QuizImage.sprite = sprite;
+            size.sizeDelta = new Vector2(697.7f, 312.6f);
         }
 
         else
