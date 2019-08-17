23a9e766ff120872e9581c7f5acc7b707c4c36f2
senshirou <senshirou@hotmail.com>
2019-08-17 14:40:02 +0900

    add function in imagecontrol

--- Assets/Script/ImageControl.cs
+++ Assets/Script/ImageControl.cs
@@ -24,42 +24,37 @@ public class ImageControl : MonoBehaviour
     {
         if (QuizNumber.text == "Quiz3" || QuizNumber.text == "Quiz4" || QuizNumber.text == "Quiz5")
         {
-            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            ImageScale255();
             sprite = Resources.Load<Sprite>("Question3_4png");
-            QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(697.7f, 312.6f);
         }
 
         else if(QuizNumber.text == "Quiz8")
         {
-            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            ImageScale255();
             sprite = Resources.Load<Sprite>("Question8");
-            QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(242.5f, 266.5f);
            
         }
 
         else if (QuizNumber.text == "Quiz9")
         {
-            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            ImageScale255();
             sprite = Resources.Load<Sprite>("Question9");
-            QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(697.7f, 312.6f);
         }
 
         else if (QuizNumber.text == "Quiz12")
         {
-            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            ImageScale255();
             sprite = Resources.Load<Sprite>("Question12");
-            QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(353.6f, 312.6f);
         }
 
-        if (QuizNumber.text == "Quiz14" || QuizNumber.text == "Quiz15")
+        else if (QuizNumber.text == "Quiz14" || QuizNumber.text == "Quiz15")
         {
-            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            ImageScale255();
             sprite = Resources.Load<Sprite>("Question14_15");
-            QuizImage.sprite = sprite;
             size.sizeDelta = new Vector2(697.7f, 312.6f);
         }
 
@@ -70,6 +65,13 @@ public class ImageControl : MonoBehaviour
         }
 
 
+
+    }
+
+    void ImageScale255()
+    {
+        QuizImage.sprite = sprite;
+        GetComponent<Image>().color = new Color32(255, 255, 255, 255);
 
     }
 
