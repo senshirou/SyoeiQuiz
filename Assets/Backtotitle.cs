4cb90281b29f5f88a8fdc4a53e6363b14e10cbc9
senshirou <senshirou@hotmail.com>
2019-09-15 23:46:58 +0900

    add a SectionScene

---
+++ Assets/Backtotitle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backtotitle : MonoBehaviour
+{
+    GameObject background;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GameObject.Find("BackGround");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Title()
+    {
+        Destroy(background);
+        SceneManager.LoadScene("PracticeSpace");
+        
+        
+    }
+}
--- Assets/TotalScore.cs
+++ Assets/TotalScore.cs
@@ -16,30 +16,38 @@ public class TotalScore : MonoBehaviour
     {
 
         Result = GetComponent<TextMeshPro>();
-        Result.text = SManager.Score.ToString() + " / " + TotalS.questionIndex.ToString();
-
+        //Result.text = SManager.Score.ToString() + " / " + TotalS.questionIndex.ToString();
+        
         //割り算をするため
         float Scorei = (float)SManager.Score;
         float TotalSS = (float)TotalS.questionIndex;
-        float CorrectP = Scorei / TotalSS;
+        float CorrectP = Scorei / TotalSS * 100;
+
+        //結果の表示
+        Result.text = CorrectP.ToString("F0") + "%";
+
 
         //判定テキスト
         switch(CorrectP)
         {
-            case float i when i == 1:
+            case float i when i == 100:
             Level.text = "完璧";
+            Level.color = Color.yellow;
             break;
-            case float i when i >= 0.75f:
+            case float i when i >= 75f:
             Level.text = "優秀";
+            Level.color = Color.blue;
             break;
-            case float i when i >= 0.50f:
+            case float i when i >= 50f:
             Level.text = "普通";
             break;
-            case float i when i >= 0.25f:
+            case float i when i >= 25f:
             Level.text = "不勉";
+            Level.color = Color.red;
             break;
             case float i when i >= 0:
             Level.text = "無知";
+            Level.color = Color.red;
             break;
             default:
             Level.text = "なし";
